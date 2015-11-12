888cd8a1bcb6bddcfa071bbfdae22b0c020d23c7
cjustice10 <cjustice@uw.edu>
2015-11-12 14:27:18 -0800

    Password and Password Confirmation working, no testing yet

--- js/app.js
+++ js/app.js
@@ -13,4 +13,25 @@ angular.module('TeamFormApp', [])
 
         $scope.submitted = true;
     };
+
+    $scope.verifyPassword = function() {
+    	if(!$scope.teamForm.confirmPassword.$pristine) {
+    		console.log($scope.confirmPassword);
+    		//Case: If confirmation password input has been touched but no password is 
+    		//      entered, invalidate input, display error to retype password
+	    	if($scope.confirmPassword === undefined) {
+	    		$scope.teamForm.confirmPassword.$setValidity("match", true);
+	    		$scope.teamForm.confirmPassword.$setValidity("text", false);
+	    	//Case: If passwords match, validate input, no errors
+	    	} else if($scope.password === $scope.confirmPassword) {
+	    		$scope.teamForm.confirmPassword.$setValidity("match", true);
+	    		$scope.teamForm.confirmPassword.$setValidity("text", true);
+	    	//Case: If confirmation password is entered, but does not match password,
+	    	//		invalidate input, display error that password do not match
+	    	} else {
+	    		$scope.teamForm.confirmPassword.$setValidity("match", false);
+	    		$scope.teamForm.confirmPassword.$setValidity("text", true);
+	    	}
+	    }
+    }
 }]);

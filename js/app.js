069cdf6740f9f7c0baa38b458756b793b9d676c4
iGuest <ctl14@uw.edu>
2015-11-12 11:10:19 -0800

    Add form and header

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 'use strict';
 
 angular.module('TeamFormApp', [])
-.controller('TeamFormCtrl', '$scope', function($scope) {
+.controller('TeamFormCtrl', ['$scope', function($scope) {
+    $scope.submitted = true;
 
-};
+    $scope.submit = function() {
+
+        $scope.submitted = false;
+    };
+
+    $scope.reset = function() {
+
+        $scope.submitted = true;
+    };
+}]);

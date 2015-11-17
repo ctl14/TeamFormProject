5d639a9501b6139f20e52f782b8efd601363eae2
iGuest <ctl14@uw.edu>
2015-11-17 14:19:26 -0800

    Add Disable Button Test for Last Name field

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -256,4 +256,25 @@ describe('Team Form page', function() {
 
 
 	})
+
+	it('should disable the submit button if the Last Name field is invalid', function() {
+		var birthdateInput = element(by.model('birthdate'));
+		var nameInput = element( by.model('lastName') );
+		var passwordInput = element( by.model('password') );
+		var confirmPasswordInput = element( by.model('confirmPassword') );
+		var emailInput = element( by.model('email') );
+		var submitButton = element( by.id('submitButton') );
+
+
+		birthdateInput.sendKeys('10/10/1010');
+		nameInput.sendKeys('');
+		passwordInput.sendKeys('password');
+		confirmPasswordInput.sendKeys('password');
+		emailInput.sendKeys('a@f.com');
+
+		expect( submitButton.isEnabled()).toEqual(false);
+
+
+	})
+
 });

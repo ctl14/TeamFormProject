14bd721cb60ffc0c11b4831f64717defb40353a2
Riley Andert <Randert81@gmail.com>
2015-11-17 13:48:32 -0800

    slight changes to birthday test

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -7,28 +7,28 @@ describe('Team Form page', function() {
 		var birthdateInput = element(by.model('birthdate'));
 		var tooYoung = element(by.id('tooYoung'));
 		birthdateInput.sendKeys('01/22/2013');
-		expect(tooYoung.isDisplayed()).toEquals(true);
+		expect(tooYoung.isDisplayed()).toEqual(true);
 	})
 
 	it('should show the need valid date error message when an invalid date is entered', function(){
 		var birthdateInput = element(by.model('birthdate'));
 		var dateInvalid = element(by.id('dateInvalid'));
 		birthdateInput.sendKeys('011/2');
-		expect(dateInvalid.isDisplayed()).toEquals(true);
+		expect(dateInvalid.isDisplayed()).toEqual(true);
 	})
 
 	it('should not show an error message when a birthdate entered is over 13', function(){
 		var birthdateInput = element(by.model('birthdate'));
 		var tooYoung = element(by.id('tooYoung'));
 		birthdateInput.sendKeys('01/22/2000');
-		expect(tooYoung.isDisplayed()).toEquals(false);
+		expect(tooYoung.isDisplayed()).toEqual(false);
 	})
 
 	it('should not show an error message when a birthdate entered is in valid form', function(){
 		var birthdateInput = element(by.model('birthdate'));
 		var dateInvalid = element(by.id('dateInvalid'));
 		birthdateInput.sendKeys('01/22/2000');
-		expect(dateInvalid.isDisplayed()).toEquals(false);
+		expect(dateInvalid.isDisplayed()).toEqual(false);
 	})
 
 	it('should display error when input is entered and cleared', function(){
@@ -37,8 +37,8 @@ describe('Team Form page', function() {
 		var tooYoung = element(by.id('tooYoung'));
 		birthdateInput.sendKeys('01/22/2000');
 		birthdateInput.clear();
-		expect(dateInvalid.isDisplayed()).toEquals(false);
-		expect(tooYoung.isDisplayed()).toEquals(false);
+		expect(dateInvalid.isDisplayed()).toEqual(false);
+		expect(tooYoung.isDisplayed()).toEqual(false);
 
 	it('should display an error message when the last name field has been touched but has no input', function() {
         var nameInput = element( by.model('lastName') );

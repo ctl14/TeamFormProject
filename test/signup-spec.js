47dc940f345e9c961bd11edd1bd915d3e486a27d
Riley Andert <Randert81@gmail.com>
2015-11-17 14:17:16 -0800

    fixing merge conflict

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 describe('Team Form page', function() {
 	beforeEach(function() {
 		browser.get('http://localhost:8000/');
@@ -7,13 +9,18 @@ describe('Team Form page', function() {
 		var birthdateInput = element(by.model('birthdate'));
 		var tooYoung = element(by.id('tooYoung'));
 		birthdateInput.sendKeys('01/22/2013');
+		var first = element( by.id('firstName') );
+		first.sendKeys('a');
 		expect(tooYoung.isDisplayed()).toEqual(true);
 	})
 
 	it('should show the need valid date error message when an invalid date is entered', function(){
 		var birthdateInput = element(by.model('birthdate'));
 		var dateInvalid = element(by.id('dateInvalid'));
-		birthdateInput.sendKeys('011/2');
+		birthdateInput.sendKeys('011/243');
+		var first = element( by.id('firstName') );
+		first.sendKeys('a');
+		browser.pause();
 		expect(dateInvalid.isDisplayed()).toEqual(true);
 	})
 
@@ -40,36 +47,48 @@ describe('Team Form page', function() {
 		expect(dateInvalid.isDisplayed()).toEqual(false);
 		expect(tooYoung.isDisplayed()).toEqual(false);
 
+	})
+
+	it('should not display an error message when it is untouched', function(){
+		var dateInvalid = element(by.id('dateInvalid'));
+		var tooYoung = element(by.id('tooYoung'));
+		expect(dateInvalid.isDisplayed()).toEqual(false);
+		expect(tooYoung.isDisplayed()).toEqual(false);
+
+	})
+
+	//////////////////////////////////////////////////
+
 	it('should display an error message when the last name field has been touched but has no input', function() {
-        var nameInput = element( by.model('lastName') );
-        var first = element( by.id('firstName') );
+		var nameInput = element( by.model('lastName') );
+		var first = element( by.id('firstName') );
 
-        nameInput.sendKeys('a');
-        nameInput.clear();
-        first.sendKeys('a');
+		nameInput.sendKeys('a');
+		nameInput.clear();
+		first.sendKeys('a');
 
-        var nameError = element( by.id('nameError') );
-        expect(nameError.isDisplayed()).toEqual(true);
-    })
+		var nameError = element( by.id('nameError') );
+		expect(nameError.isDisplayed()).toEqual(true);
+	})
 
-    it('should not display an error message when Last Name has been touched', function() {
-        var first = element( by.id('firstName') );
-        first.sendKeys('a');
+	it('should not display an error message when Last Name has been touched', function() {
+		var first = element( by.id('firstName') );
+		first.sendKeys('a');
 
-        var nameError = element( by.id('nameError') );
-        expect(nameError.isDisplayed()).toEqual(false);
-    })
+		var nameError = element( by.id('nameError') );
+		expect(nameError.isDisplayed()).toEqual(false);
+	})
 
-    it('should not display an error message when the last name field has an input', function() {
-        var nameInput = element( by.model('lastName') );
-        var first = element( by.id('firstName') );
+	it('should not display an error message when the last name field has an input', function() {
+		var nameInput = element( by.model('lastName') );
+		var first = element( by.id('firstName') );
 
-        nameInput.sendKeys('a');
-        first.sendKeys('a');
+		nameInput.sendKeys('a');
+		first.sendKeys('a');
 
-        var nameError = element( by.id('nameError') );
-        expect(nameError.isDisplayed()).toEqual(false);
-    })
+		var nameError = element( by.id('nameError') );
+		expect(nameError.isDisplayed()).toEqual(false);
+	})
 
 
 	it('should not display an error before the password input is touched', function() {
@@ -111,18 +130,7 @@ describe('Team Form page', function() {
 	it('should not display an error befor the confirmation password input is touched', function() {
 		var noConfirmationPasswordError = element( by.id('noConfirmationPasswordError') );
 		expect( noConfirmationPasswordError.isDisplayed() ).toEqual(false);
-	})
 
-	it('should not display an error message when it is untouched', function(){
-		var dateInvalid = element(by.id('dateInvalid'));
-		var tooYoung = element(by.id('tooYoung'));
-		expect(dateInvalid.isDisplayed()).toEquals(false);
-		expect(tooYoung.isDisplayed()).toEquals(false);
-
-	})
-
-		var submitButton = element( by.id('submitButton') );
-		expect( submitButton.isEnabled() ).toEqual(false);
 	})
 
 	it('should display an error message when just a confirmation password is entered', function() {
@@ -134,7 +142,7 @@ describe('Team Form page', function() {
 		expect( noMatchError.isDisplayed() ).toEqual(true);
 	})
 
-	it('should disable the submit button when just a confirmation password is entered then delted', function() {
+	it('should disable the submit button when just a confirmation password is entered then deleted', function() {
 		var confirmPasswordInput = element( by.model('confirmPassword') );
 
 		confirmPasswordInput.sendKeys('password');
@@ -142,7 +150,7 @@ describe('Team Form page', function() {
 
 		var submitButton = element( by.id('submitButton') );
 		expect( submitButton.isEnabled() ).toEqual(false);
-	})			
+	})
 
 	it('should display an error message when just a confirmation password is entered then deleted', function() {
 		var confirmPasswordInput = element( by.model('confirmPassword') );
@@ -158,7 +166,7 @@ describe('Team Form page', function() {
 
 	it('should disable the submit button when the password and confirmation password do not match', function() {
 		var passwordInput = element( by.model('password') );
-		var confirmPasswordInput = element( by.model('confirmPassword') ); 
+		var confirmPasswordInput = element( by.model('confirmPassword') );
 
 		passwordInput.sendKeys('password');
 		confirmPasswordInput.sendKeys('nomatch');
@@ -176,49 +184,53 @@ describe('Team Form page', function() {
 		confirmPasswordInput.sendKeys('nomatch');
 
 		expect( noMatchError.isDisplayed() ).toEqual(true);
-	})	
+	})
 
 	//EMAIL TESTS
 	//The user entered a valid email
 	it('should not say: This is not a valid email address', function() {
 		var emailInput = element( by.model('email') );
-		var noAtSignMsg = element( by.id('noAtSignMsg') ); 
+		var noAtSignMsg = element( by.id('noAtSignMsg') );
 
 		emailInput.sendKeys('husky@uw.edu');
 
 
-		expect( !noAtSignMsg.isDisplayed() );
+		expect( noAtSignMsg.isDisplayed()).toEqual(false);
 	})
 	//The user entered a valid email
 	it('should not say: You must enter a valid email', function() {
 		var emailInput = element( by.model('email') );
-		var noInputMsg = element( by.id('noInputMsg') ); 
+		var noInputMsg = element( by.id('noInputMsg') );
 
 		emailInput.sendKeys('husky@uw.edu');
 
 
-		expect( !noInputMsg.isDisplayed() );
+		expect( noInputMsg.isDisplayed() ).toEqual(false);
 	})
 
 	//The user did not input an @ sign
 	it('should activate: This is not a valid email address', function() {
 		var emailInput = element( by.model('email') );
-		var noAtSignMsg = element( by.id('noAtSignMsg') ); 
+		var noAtSignMsg = element( by.id('noAtSignMsg') );
 
 		emailInput.sendKeys('husky');
 
 
-		expect( noAtSignMsg.isDisplayed() );
+		expect( noAtSignMsg.isDisplayed() ).toEqual(true);
 	})
 
 	//The user did not enter anything
 	it('should activate: You must enter a valid email', function() {
 		var emailInput = element( by.model('email') );
-		var noInputMsg = element( by.id('noInputMsg') ); 
+		var noInputMsg = element( by.id('noInputMsg') );
 
 		emailInput.sendKeys('');
 
+		//Select outside of the email input, so the message shows up
+		var first = element( by.id('firstName') );
+		first.sendKeys('name');
 
-		expect( noInputMsg.isDisplayed() );
+
+		expect( noInputMsg.isDisplayed() ).toEqual(true);
 	})
 });

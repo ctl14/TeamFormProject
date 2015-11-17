36dd903f8b9c9cd1ac5374bf1e922de2d056c132
cjustice10 <cjustice@uw.edu>
2015-11-17 13:42:09 -0800

    added assertions to protractor tests

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -31,9 +31,9 @@ describe('Team Form page', function() {
 
 	//////////////////////////////////////////////////////
 
-	it('should not display an error befor the password input is touched', function() {
+	it('should not display an error before the password input is touched', function() {
 		var noPasswordError = element( by.id('noPasswordError') );
-		expect( !noPasswordError.isDisplayed() );
+		expect( noPasswordError.isDisplayed() ).toEqual(false);
 	})
 
 	it('should disable the submit button when just a password is entered', function() {
@@ -42,7 +42,7 @@ describe('Team Form page', function() {
 		passwordInput.sendKeys('password');
 
 		var submitButton = element( by.id('submitButton') );
-		expect( !submitButton.isEnabled() );
+		expect( submitButton.isEnabled() ).toEqual(false);
 	})
 
 	it('should disable the submit button when just a password is entered then deleted', function() {
@@ -52,22 +52,24 @@ describe('Team Form page', function() {
 		passwordInput.clear();
 
 		var submitButton = element( by.id('submitButton'));
-		expect( !submitButton.isEnabled() );
+		expect( submitButton.isEnabled() ).toEqual(false);
 	})
 
 	it('should display an error message when just a password is entered then deleted', function() {
 		var passwordInput = element( by.model('password') );
 		var noPasswordError = element( by.id('noPasswordError') );
+		var first = element( by.id('firstName') );
 
 		passwordInput.sendKeys('password');
 		passwordInput.clear();
+		first.sendKeys(' ');
 
-		expect( noPasswordError.isDisplayed() );
+		expect( noPasswordError.isDisplayed() ).toEqual(true);
 	})
 
 	it('should not display an error befor the confirmation password input is touched', function() {
 		var noConfirmationPasswordError = element( by.id('noConfirmationPasswordError') );
-		expect( !noConfirmationPasswordError.isDisplayed() );
+		expect( noConfirmationPasswordError.isDisplayed() ).toEqual(false);
 	})
 
 	it('should disable the submit button when just a confirmation password is entered', function() {
@@ -76,7 +78,7 @@ describe('Team Form page', function() {
 		confirmPasswordInput.sendKeys('password');
 
 		var submitButton = element( by.id('submitButton') );
-		expect( !submitButton.isEnabled() );
+		expect( submitButton.isEnabled() ).toEqual(false);
 	})
 
 	it('should display an error message when just a confirmation password is entered', function() {
@@ -85,7 +87,7 @@ describe('Team Form page', function() {
 
 		confirmPasswordInput.sendKeys('password');
 
-		expect( noMatchError.isDisplayed() );
+		expect( noMatchError.isDisplayed() ).toEqual(true);
 	})
 
 	it('should disable the submit button when just a confirmation password is entered then delted', function() {
@@ -95,17 +97,19 @@ describe('Team Form page', function() {
 		confirmPasswordInput.clear();
 
 		var submitButton = element( by.id('submitButton') );
-		expect( !submitButton.isEnabled() );
+		expect( submitButton.isEnabled() ).toEqual(false);
 	})			
 
 	it('should display an error message when just a confirmation password is entered then deleted', function() {
 		var confirmPasswordInput = element( by.model('confirmPassword') );
 		var noConfirmationPasswordError = element( by.id('noConfirmationPasswordError') );
+		var first = element( by.id('firstName') );
 
 		confirmPasswordInput.sendKeys('password');
 		confirmPasswordInput.clear();
+		first.sendKeys(' ');
 
-		expect( noConfirmationPasswordError.isDisplayed() );
+		expect( noConfirmationPasswordError.isDisplayed() ).toEqual(true);
 	})
 
 	it('should disable the submit button when the password and confirmation password do not match', function() {
@@ -116,7 +120,7 @@ describe('Team Form page', function() {
 		confirmPasswordInput.sendKeys('nomatch');
 
 		var submitButton = element( by.id('submitButton'));
-		expect( !submitButton.isEnabled() );
+		expect( submitButton.isEnabled() ).toEqual(false);
 	})
 
 	it('should display an error message when the password and confirmation password do not match', function() {
@@ -127,17 +131,7 @@ describe('Team Form page', function() {
 		passwordInput.sendKeys('password');
 		confirmPasswordInput.sendKeys('nomatch');
 
-		expect( noMatchError.isDisplayed() );
+		expect( noMatchError.isDisplayed() ).toEqual(true);
 	})	
 
-	it('should enable the submit button when the password and confirmation password match', function() {
-		var passwordInput = element( by.model('password') );
-		var confirmPasswordInput = element( by.model('confirmPassword') ); 
-
-		passwordInput.sendKeys('password');
-		confirmPasswordInput.sendKeys('password');
-
-		var submitButton = element( by.id('submitButton'));
-		expect( submitButton.isEnabled() );
-	})
 });

3a1560a569d0cc25de785ee66b8f618eb877fc44
Riley Andert <Randert81@gmail.com>
2015-11-17 13:40:13 -0800

    adds birthday validation

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -29,7 +29,6 @@ describe('Team Form page', function() {
         expect(!nameError.isDisplayed());
     })
 
-	//////////////////////////////////////////////////////
 
 	it('should not display an error befor the password input is touched', function() {
 		var noPasswordError = element( by.id('noPasswordError') );
@@ -183,7 +182,53 @@ describe('Team Form page', function() {
 
 		emailInput.sendKeys('');
 
-
 		expect( noInputMsg.isDisplayed() );
 	})
+
+	it('should show the under 13 error message when a birthday entered is under 13', function(){
+		var birthdateInput = element(by.model('birthdate'));
+		var tooYoung = element(by.id('tooYoung'));
+		birthdateInput.sendKeys('01/22/2013');
+		expect(tooYoung.isDisplayed() );
+	})
+
+	it('should show the need valid date error message when an invalid date is entered', function(){
+		var birthdateInput = element(by.model('birthdate'));
+		var dateInvalid = element(by.id('dateInvalid'));
+		birthdateInput.sendKeys('011/2');
+		expect(dateInvalid.isDisplayed() );
+	})
+
+	it('should not show an error message when a birthdate entered is over 13', function(){
+		var birthdateInput = element(by.model('birthdate'));
+		var tooYoung = element(by.id('tooYoung'));
+		birthdateInput.sendKeys('01/22/2000');
+		expect(!tooYoung.isDisplayed() );
+	})
+
+	it('should not show an error message when a birthdate entered is in valid form', function(){
+		var birthdateInput = element(by.model('birthdate'));
+		var dateInvalid = element(by.id('dateInvalid'));
+		birthdateInput.sendKeys('01/22/2000');
+		expect(!dateInvalid.isDisplayed() );
+	})
+
+	it('should display error when input is entered and cleared', function(){
+		var birthdateInput = element(by.model('birthdate'));
+		var dateInvalid = element(by.id('dateInvalid'));
+		var tooYoung = element(by.id('tooYoung'));
+		birthdateInput.sendKeys('01/22/2000');
+		birthdateInput.clear();
+		expect(!dateInvalid.isDisplayed() );
+		expect(!tooYoung.isDisplayed());
+	})
+
+	it('should not display an error message when it is untouched', function(){
+		var dateInvalid = element(by.id('dateInvalid'));
+		var tooYoung = element(by.id('tooYoung'));
+		expect(!dateInvalid.isDisplayed() );
+		expect(!tooYoung.isDisplayed());
+
+	})
+
 });
